ab527ddd73be53c498f7b1f715292ff3633a5ba4
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-02-20 13:04:59 +0500

    work with labels

---
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Bektemur Mamashayev on 15/02/23.
+//
+
+import UIKit
+
+final class QuestionViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+    
+}
+
+ 
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Bektemur Mamashayev on 20/02/23.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
